0c35d1c34b024f9be1ab4a47ddf45b39915fac09
logumakesh <logaveni@gmail.com>
2021-08-28 20:24:16 +0530

    practise1

--- src/org/all/Languageclass.java
+++ src/org/all/Languageclass.java
@@ -1,10 +1,8 @@
 package org.all;
-
-
-
 public class Languageclass   {
 	private void alllanguage() {
-		System.out.println("all language");
+		System.out.println("all language is in one class");
+		System.out.println("checking");
 
 	}
 	public static void main(String[] args) {
